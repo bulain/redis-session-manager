a8eb4e7751701bf366bf75aac7a869e3445cd015
Bulain Peng <bulain@gmail.com>
2017-05-15 00:16:12 +0800

    fix serialization Strategy

--- tc7/src/main/java/com/bulain/tomcat7/redissessions/RedisSessionManager.java
+++ tc7/src/main/java/com/bulain/tomcat7/redissessions/RedisSessionManager.java
@@ -69,7 +69,7 @@ public class RedisSessionManager extends ManagerBase implements Lifecycle {
 
     protected static String name = "RedisSessionManager";
 
-    protected String serializationStrategyClass = "com.orangefunction.tomcat.redissessions.JavaSerializer";
+    protected String serializationStrategyClass = "com.bulain.tomcat7.redissessions.JavaSerializer";
 
     protected EnumSet<SessionPersistPolicy> sessionPersistPoliciesSet = EnumSet.of(SessionPersistPolicy.DEFAULT);
 
